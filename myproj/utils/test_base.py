a6cd41d84d6144dcbfac97fa55fb9f908fad9c4d
Henry Medina <seth.enzo.medina@gmail.com>
2020-08-27 18:02:35 +0000

    re-factored upload to uploads

--- myproj/utils/test_base.py
+++ myproj/utils/test_base.py
@@ -151,7 +151,7 @@ class UploadMixin(MyTestCase):
         data['file'] = uploaded_chunk
 
         response = self.client.post(
-            '/api/v1/upload/' % VERSION, data, format='multipart'
+            '/api/v1/uploads/' % VERSION, data, format='multipart'
         )
         json_response = response.json()
         LOG.info(json_response)
