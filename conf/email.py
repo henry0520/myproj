3d9c5ea083d227a53e59ed685f16cad5f4409723
Henry Medina <seth.enzo.medina@gmail.com>
2020-08-27 17:46:53 +0000

    re-factored email

--- conf/email.py
+++ conf/email.py
@@ -4,6 +4,7 @@
 #EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 #SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
+# Remove the configuration below once we have the send grid account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
