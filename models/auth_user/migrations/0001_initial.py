2097eb6185d0dbdd5a957a806acba172bb3d9cc9
Henry Medina <seth.enzo.medina@gmail.com>
2020-08-28 17:44:29 +0000

    re-factored migrations

--- models/auth_user/migrations/0001_initial.py
+++ models/auth_user/migrations/0001_initial.py
@@ -2,7 +2,19 @@
 
 import django.contrib.auth.models
 from django.db import migrations
+from django.core.management import call_command
 
+def load_initial_user(apps, schema_editor):
+    """
+    load initial user
+    """
+    User = apps.get_model("auth_user", "User")
+    user, _ = User.objects.get_or_create(
+        email='testuser@venn.bio',
+        username='testuser')
+    user.set_password('1234')
+    user.is_superuser = True
+    user.is_staff = True
 
 class Migration(migrations.Migration):
 
@@ -27,4 +39,5 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
+        migrations.RunPython(load_initial_user),
     ]
--- models/upload/migrations/0001_initial.py
+++ models/upload/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('date_created', models.DateTimeField(db_index=True, default=django.utils.timezone.now)),
                 ('uploaded_chunks', jsonfield.fields.JSONField(default=dict)),
                 ('uploaded_size', models.BigIntegerField(null=True)),
-                ('batch', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='upload', to='batch.Batch')),
+                ('batch', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='uploads', to='batch.Batch')),
                 ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='auth_user.User')),
             ],
             options={
