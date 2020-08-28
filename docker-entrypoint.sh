02669c45614f31873a84b20735454f394c58b8a1
Henry Medina <seth.enzo.medina@gmail.com>
2020-08-28 17:00:09 +0000

    added back docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,3 +3,4 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('testuser', 'testuser@venn.bio', '1234')" | python manage.py shell
+python manage.py runserver 0.0.0.0:8000
